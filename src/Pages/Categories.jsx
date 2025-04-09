96e5a71a058bd4d5daeea6e2d9ee939dc620b5e6
N-meapp <developersnme@gmail.com>
2025-04-09 17:09:37 +0530

    public

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,5 +1,5 @@
-import logo from '../assets/Images/logo/image 94.png'
-import profile from '../assets/Images/profile/profile-1.jpg'
+// import logo from ''
+// import profile from '../assets/Images/profile/profile-1.jpg'
 
 
 export default function Categories(){
@@ -7,12 +7,16 @@ export default function Categories(){
         <>
         <div className="w-[80%] mx-auto h-[60px] mt-[69px] flex justify-between" 
         c>
-            <img className="h-fit" src={logo} ></img>
+            <img className="h-fit" src='/assets/Images/logo/image 94.png' ></img>
             <div className='w-[60px] rounded-full'>
-            <img className="w-full h-full rounded-full object-cover" src={profile} ></img>
+            <img className="w-full h-full rounded-full object-cover" src='/assets/Images/profile/profile-1.jpg' ></img>
             </div>
         </div>
-        <div className='mt-[101px] h-[341px] bg-[red] rounded-[20px]'>
+        <div className='mt-[101px] h-[341px] bg-[red] rounded-[20px]'  style={{
+    backgroundImage: 'url("/assets/Images/offers/offer-four.jpg")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }}>
 
         </div>
         </>
